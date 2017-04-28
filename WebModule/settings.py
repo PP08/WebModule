3d8a4a2491fbee80c5929523ceb6f7097e9932dd
phucphuong <phucphuong1293@gmail.com>
2017-04-28 23:58:48 +0300

    configured for heroku

--- WebModule/settings.py
+++ WebModule/settings.py
@@ -127,9 +127,9 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
